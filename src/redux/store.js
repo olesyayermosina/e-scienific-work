638311d70e1dcdfb3641843aece8865903fb2fe0
user <irchuk67>
2024-09-17 17:12:09 +0300

    add: state management during merging process, display upploaded filename

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {logger} from "redux-logger/src";
+import {mergingSlice} from "./slices/mergingSlice.js";
 
 const store = configureStore({
     reducer: {
-
+        merging: mergingSlice.reducer
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger)
 });
